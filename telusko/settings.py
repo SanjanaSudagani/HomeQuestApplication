bd9925ad64e159248604b968abdc2aa60c67c0b9
NirupamYashas <48911001+NirupamYashas@users.noreply.github.com>
2023-04-22 17:19:22 -0400

    migrate changes to heroku

--- telusko/settings.py
+++ telusko/settings.py
@@ -15,6 +15,8 @@ import django_heroku
 import dj_database_url
 from django.core.management.utils import get_random_secret_key
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
