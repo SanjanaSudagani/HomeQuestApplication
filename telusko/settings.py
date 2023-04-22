0c4f781d523c35481ae053798d045e0cb30144c9
NirupamYashas <48911001+NirupamYashas@users.noreply.github.com>
2023-04-22 16:42:09 -0400

    Update DATABASES setting

--- telusko/settings.py
+++ telusko/settings.py
@@ -81,11 +81,15 @@ WSGI_APPLICATION = 'telusko.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3'),
 }
 
 
